d69cf14fd0ffcff2a34f02bfddd400878a9705c9
wangzijie <wangzijie@360.net>
2019-12-08 15:01:04 +0800

    add hbase http tunny wrapper

--- http/http.go
+++ http/http.go
@@ -19,6 +19,26 @@ func (c *Client) Init() {
 	}
 }
 
+func (c *Client) Get(url string) (string, error) {
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+	resp, err := c.client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("%+v", resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
 func (c *Client) PostParams(url string, params map[string]string) (string, error) {
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
@@ -31,25 +51,30 @@ func (c *Client) PostParams(url string, params map[string]string) (string, error
 	}
 
 	request, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return "", err
+	}
 	request.Header.Set("Content-Type", writer.FormDataContentType())
 	resp, err := c.client.Do(request)
 	if err != nil {
 		return "", err
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 		return "", fmt.Errorf("%+v", resp.Status)
 	}
-	rbody := &bytes.Buffer{}
-	_, err = rbody.ReadFrom(resp.Body)
-	defer resp.Body.Close()
+	resBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
-	return rbody.String(), nil
+	return string(resBody), nil
 }
 
 func (c *Client) PostJson(url string, jsonByte []byte) (string, error) {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonByte))
+	if err != nil {
+		return "", err
+	}
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := c.client.Do(req)
---
+++ pool/go.mod
@@ -0,0 +1,5 @@
+module tunny_pool
+
+go 1.12
+
+require github.com/Jeffail/tunny v0.0.0-20190930221602-f13eb662a36a
